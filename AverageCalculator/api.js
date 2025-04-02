4ea8acfe7e02b8bd10be35d17005bfab32c66711
Rishi's git <devanshtiwari2426@gmail.com>
2025-04-02 19:48:39 +0530

    gitignore added

---
+++ AverageCalculator/api.js
@@ -0,0 +1,89 @@
+const express = require("express");
+const axios = require("axios");
+
+const apiKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJNYXBDbGFpbXMiOnsiZXhwIjoxNzQzNjAzNTE2LCJpYXQiOjE3NDM2MDMyMTYsImlzcyI6IkFmZm9yZG1lZCIsImp0aSI6ImY5ZjUzMGZlLThmMGQtNDI0Mi05YzdkLTQ2MWJhNWIzYWM4ZSIsInN1YiI6IjIyMDUyMDZAa2lpdC5hYy5pbiJ9LCJlbWFpbCI6IjIyMDUyMDZAa2lpdC5hYy5pbiIsIm5hbWUiOiJkZXZhbnNoIHRpd2FyaSIsInJvbGxObyI6IjIyMDUyMDYiLCJhY2Nlc3NDb2RlIjoibndwd3JaIiwiY2xpZW50SUQiOiJmOWY1MzBmZS04ZjBkLTQyNDItOWM3ZC00NjFiYTViM2FjOGUiLCJjbGllbnRTZWNyZXQiOiJmcVF0RFNKRkVUcVV0RkZoIn0.Mxt_TiOexm7zIjGZEsPNZPwTXf8vvLAKbOHa7oHIBWE';
+
+const app = express();
+const portNumber = 3000;
+
+app.listen(portNumber, function() {
+  console.log("Server is running on port " + portNumber);
+});
+
+const windowSize = 10;
+const apiUrls = {
+    p: "http://20.244.56.144/evaluation-service/primes",
+    f: "http://20.244.56.144/evaluation-service/fibo",
+    e: "http://20.244.56.144/evaluation-service/even",
+    r: "http://20.244.56.144/evaluation-service/random"
+};
+
+const numberStorageData = {
+  p: [],
+  f: [],
+  e: [],
+  r: []
+};
+
+async function getNumberList(type) {
+  try {
+    const apiResponse = await axios.get(apiUrls[type], {
+      timeout: 500,
+      headers: { Authorization: "Bearer " + apiKey }
+    });
+    if(apiResponse.data && apiResponse.data.numbers) {
+      return apiResponse.data.numbers;
+    } else {
+      return [];
+    }
+  } catch (error) {
+    return [];
+  }
+}
+
+app.get("/numbers/:type", async function(request, response) {
+  const requestType = request.params.type;
+  if (!apiUrls[requestType]) {
+    return response.status(400).json({ error: "Invalid type. Use 'p', 'f', 'e', or 'r'." });
+  }
+
+  let previousState = [];
+  for(let i = 0; i < numberStorageData[requestType].length; i++){
+    previousState.push(numberStorageData[requestType][i]);
+  }
+
+  const freshNumbers = await getNumberList(requestType);
+
+  if (freshNumbers.length > 0) {
+    for (const number of freshNumbers) {
+      if (numberStorageData[requestType].indexOf(number) === -1) {
+        numberStorageData[requestType].push(number);
+        if (numberStorageData[requestType].length > windowSize) {
+          numberStorageData[requestType].shift();
+        }
+      }
+    }
+  }
+
+  let totalSum = 0;
+  for(let i = 0; i < numberStorageData[requestType].length; i++){
+    totalSum = totalSum + numberStorageData[requestType][i];
+  }
+
+  let averageValue = "0.00";
+  if (numberStorageData[requestType].length > 0) {
+    averageValue = (totalSum / numberStorageData[requestType].length).toFixed(2);
+  }
+
+  let currentState = [];
+  for(let i = 0; i < numberStorageData[requestType].length; i++){
+    currentState.push(numberStorageData[requestType][i]);
+  }
+
+  response.json({
+    windowPrevState: previousState,
+    windowCurrState: currentState,
+    numbers: freshNumbers,
+    avg: parseFloat(averageValue)
+});
+});
---
+++ SocialMediaService/api.js
@@ -0,0 +1,100 @@
+const express = require("express");
+const axios = require("axios");
+
+const app = express();
+const portNumber = 3000;
+const apiBaseAddress = "http://20.244.56.144/evaluation-service";
+const accessTokenValue ='eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJNYXBDbGFpbXMiOnsiZXhwIjoxNzQzNjAzNTE2LCJpYXQiOjE3NDM2MDMyMTYsImlzcyI6IkFmZm9yZG1lZCIsImp0aSI6ImY5ZjUzMGZlLThmMGQtNDI0Mi05YzdkLTQ2MWJhNWIzYWM4ZSIsInN1YiI6IjIyMDUyMDZAa2lpdC5hYy5pbiJ9LCJlbWFpbCI6IjIyMDUyMDZAa2lpdC5hYy5pbiIsIm5hbWUiOiJkZXZhbnNoIHRpd2FyaSIsInJvbGxObyI6IjIyMDUyMDYiLCJhY2Nlc3NDb2RlIjoibndwd3JaIiwiY2xpZW50SUQiOiJmOWY1MzBmZS04ZjBkLTQyNDItOWM3ZC00NjFiYTViM2FjOGUiLCJjbGllbnRTZWNyZXQiOiJmcVF0RFNKRkVUcVV0RkZoIn0.Mxt_TiOexm7zIjGZEsPNZPwTXf8vvLAKbOHa7oHIBWE';
+
+const requestHeaders = {
+  Authorization: "Bearer " + accessTokenValue,
+  "Content-Type": "application/json",
+};
+
+app.get("/users", async function(request, response) {
+  try {
+    const usersApiResponse = await axios.get(apiBaseAddress + "/users", { headers: requestHeaders });
+    const usersData = usersApiResponse.data.users;
+
+    const postCountsData = {};
+    for (const userId in usersData) {
+      const postsApiResponse = await axios.get(apiBaseAddress + "/users/" + userId + "/posts", { headers: requestHeaders });
+      postCountsData[userId] = postsApiResponse.data.posts.length;
+    }
+
+    let topUsersList = [];
+    let usersAndCounts = [];
+
+    for (const userId in postCountsData) {
+      usersAndCounts.push([userId, postCountsData[userId]]);
+    }
+
+    usersAndCounts.sort(function(a, b) {
+      return b[1] - a[1];
+    });
+
+    let topFive = usersAndCounts.slice(0, 5);
+
+    for (const userCount of topFive) {
+      let userId = userCount[0];
+      let postCount = userCount[1];
+      let userName = usersData[userId];
+
+      topUsersList.push({ id: userId, name: userName, postCount: postCount });
+    }
+
+    response.json({ topUsers: topUsersList });
+
+  } catch (error) {
+    response.status(500).json({ error: error.message });
+  }
+});
+
+app.get("/posts", async function(request, response) {
+  try {
+    let requestedType = request.query.type;
+    if (!requestedType || (requestedType !== "latest" && requestedType !== "popular")) {
+      return response.status(400).json({ error: "Invalid type parameter" });
+    }
+
+    const usersApiResponse = await axios.get(apiBaseAddress + "/users", { headers: requestHeaders });
+    const allUserIds = Object.keys(usersApiResponse.data.users);
+
+    let allPosts = [];
+    for (const userId of allUserIds) {
+      const postsApiResponse = await axios.get(apiBaseAddress + "/users/" + userId + "/posts", { headers: requestHeaders });
+      allPosts = allPosts.concat(postsApiResponse.data.posts);
+    }
+
+    if (requestedType === "latest") {
+      allPosts.sort(function(a, b) {
+        return b.id - a.id;
+      });
+      let latestFive = allPosts.slice(0, 5);
+      return response.json({ latestPosts: latestFive });
+    }
+
+    let popularPostsFound = [];
+    let mostComments = 0;
+    for (const post of allPosts) {
+      const commentsApiResponse = await axios.get(apiBaseAddress + "/posts/" + post.id + "/comments", { headers: requestHeaders });
+      let commentCount = commentsApiResponse.data.comments.length;
+
+      if (commentCount > mostComments) {
+        mostComments = commentCount;
+        popularPostsFound = [post];
+      } else if (commentCount === mostComments) {
+        popularPostsFound.push(post);
+      }
+    }
+
+    response.json({ popularPosts: popularPostsFound });
+
+  } catch (error) {
+    response.status(500).json({ error: error.message });
+  }
+});
+
+app.listen(portNumber, function() {
+  console.log("Server running on port " + portNumber);
+});
